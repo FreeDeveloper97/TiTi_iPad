522d66faa8ef540bf6c5215da28b9622309b5c80
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-02-02 14:02:05 +0900

    2/2 refactor : 아이패드용 제작 시작

---
+++ Project_Timer/SetTimerViewController.swift
@@ -0,0 +1,139 @@
+//
+//  SetTimerViewController.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2020/10/21.
+//  Copyright © 2020 FDEE. All rights reserved.
+//
+
+import UIKit
+
+class SetTimerViewController: UIViewController {
+    
+    @IBOutlet var Label_timer: UILabel!
+    @IBOutlet var Text_H: UITextField!
+    @IBOutlet var Text_M: UITextField!
+    @IBOutlet var Text_S: UITextField!
+    @IBOutlet var Button_set: UIButton!
+    @IBOutlet var Button_Back: UIButton!
+    
+    @IBOutlet var Label_toTime: UILabel!
+    
+    var SetTimerViewControllerDelegate : ChangeViewController!
+    
+    var H = ""
+    var M = ""
+    var S = ""
+    var h: Int = 0
+    var m: Int = 0
+    var s: Int = 0
+    var second: Int = 2400
+    
+    let BLUE = UIColor(named: "Blue")
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboard()
+        
+        second = UserDefaults.standard.value(forKey: "second") as? Int ?? 2400
+        Label_timer.text = printTime(temp: second)
+        
+        Text_H.keyboardType = .numberPad
+        Text_M.keyboardType = .numberPad
+        Text_S.keyboardType = .numberPad
+        
+        Button_set.layer.cornerRadius = 10
+        Button_set.layer.borderWidth = 3
+        Button_set.layer.borderColor = BLUE?.cgColor
+        
+        Button_Back.layer.cornerRadius = 10
+        Button_Back.layer.borderWidth = 3
+        Button_Back.layer.borderColor = UIColor.white.cgColor
+
+        Text_H.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        Text_M.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        Text_S.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        
+        //종료예정시간 보이기
+        Label_toTime.text = getFutureTime()
+    }
+    @objc func textFieldDidChange(textField: UITextField){
+        H = Text_H.text!
+        M = Text_M.text!
+        S = Text_S.text!
+        
+        check()
+        second = h * 3600 + m * 60 + s
+        Label_timer.text = printTime(temp: second)
+        Label_toTime.text = getFutureTime()
+    }
+    
+    func check()
+    {
+        if (H != "")
+        {
+            h = Int(H)!
+            m = 0
+            s = 0
+        }
+        if (M != "")
+        {
+            if(H == "")
+            {
+                h = 0
+            }
+            m = Int(M)!
+            s = 0
+        }
+        if (S != "")
+        {
+            if(H == "")
+            {
+                h = 0
+            }
+            if(M == "")
+            {
+                m = 0
+            }
+            s = Int(S)!
+        }
+    }
+    
+    func printTime(temp : Int) -> String
+    {
+        let S = temp%60
+        let H = temp/3600
+        let M = temp/60 - H*60
+        
+        let stringS = S<10 ? "0"+String(S) : String(S)
+        let stringM = M<10 ? "0"+String(M) : String(M)
+        
+        let returnString  = String(H) + ":" + stringM + ":" + stringS
+        return returnString
+    }
+    
+    @IBAction func Button_set(_ sender: UIButton) {
+        UserDefaults.standard.set(second, forKey: "second")
+        print("set complite")
+        SetTimerViewControllerDelegate.changeTimer()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func Button_Back_action(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func getFutureTime() -> String
+    {
+        //log 날짜 설정
+        let now = Date()
+        let future = now.addingTimeInterval(TimeInterval(second))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm a"
+        let today = dateFormatter.string(from: future)
+        return today
+    }
+    
+}
---
+++ Project_Timer/SetViewController.swift
@@ -0,0 +1,245 @@
+//
+//  SetViewController.swift
+//  Project_Timer
+//
+//  Created by Min_MacBook Pro on 2020/06/10.
+//  Copyright © 2020 FDEE. All rights reserved.
+//
+
+import UIKit
+
+protocol ChangeViewController {
+    func updateViewController()
+    func changeTimer()
+}
+
+class SetViewController: UIViewController {
+
+    @IBOutlet var SetButton: UIButton!
+    @IBOutlet var BackButton: UIButton!
+    
+    @IBOutlet var H1TextField: UITextField!
+    @IBOutlet var M1TextField: UITextField!
+    @IBOutlet var S1TextField: UITextField!
+    @IBOutlet var H2TextField: UITextField!
+    @IBOutlet var M2TextField: UITextField!
+    @IBOutlet var S2TextField: UITextField!
+    
+    @IBOutlet var AllTimeLabel: UILabel!
+    @IBOutlet var SecondLabel: UILabel!
+    
+    @IBOutlet var Control_persent: UISegmentedControl!
+    
+    var setViewControllerDelegate : ChangeViewController!
+    var allTime : Int = 21600
+    var second : Int = 2400
+    //빡공률 보이기
+    var showPersent: Int = 0
+    
+    var H1 = ""
+    var M1 = ""
+    var H2 = ""
+    var M2 = ""
+    var S1 = ""
+    var S2 = ""
+    var h1 = 6
+    var h2 = 0
+    var m1 = 0
+    var m2 = 40
+    var s1 = 0
+    var s2 = 0
+    
+    let BLUE = UIColor(named: "Blue")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboard()
+        
+        allTime = UserDefaults.standard.value(forKey: "allTime") as? Int ?? 21600
+        AllTimeLabel.text = printTime(temp: allTime)
+        second = UserDefaults.standard.value(forKey: "second") as? Int ?? 2400
+        SecondLabel.text = printTime(temp: second)
+        getHMS1()
+        getHMS2()
+        
+        showPersent = UserDefaults.standard.value(forKey: "showPersent") as? Int ?? 0
+        
+        H1TextField.keyboardType = .numberPad
+        M1TextField.keyboardType = .numberPad
+        H2TextField.keyboardType = .numberPad
+        M2TextField.keyboardType = .numberPad
+        S1TextField.keyboardType = .numberPad
+        S2TextField.keyboardType = .numberPad
+        
+        SetButton.layer.cornerRadius = 10
+        SetButton.layer.borderWidth = 3
+        SetButton.layer.borderColor = BLUE?.cgColor
+        
+        BackButton.layer.cornerRadius = 10
+        BackButton.layer.borderWidth = 3
+        BackButton.layer.borderColor = UIColor.white.cgColor
+        
+        H1TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        M1TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        H2TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        M2TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        S1TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        S2TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        
+        Control_persent.selectedSegmentIndex = showPersent
+    }
+    
+    @objc func textFieldDidChange(textField: UITextField){
+        H1 = H1TextField.text!
+        H2 = H2TextField.text!
+        M1 = M1TextField.text!
+        M2 = M2TextField.text!
+        S1 = S1TextField.text!
+        S2 = S2TextField.text!
+        
+        check()
+        
+        allTime = h1 * 3600 + m1 * 60 + s1
+        second = h2 * 3600 + m2 * 60 + s2
+        
+        AllTimeLabel.text = printTime(temp: allTime)
+        SecondLabel.text = printTime(temp: second)
+    }
+    
+    @IBAction func SetButton(_ sender: UIButton) {
+        //경고창 추가
+        let alert = UIAlertController(title:"SET 하시겠습니까?",message: "누적시간이 초기화되며 새로운 기록이 시작됩니다!",preferredStyle: UIAlertController.Style.alert)
+        let cancel = UIAlertAction(title: "CANCEL", style: .destructive, handler: nil)
+        let okAction = UIAlertAction(title: "SET", style: .default, handler:
+                                        {
+                                            action in
+                                            self.SET_action()
+                                        })
+        alert.addAction(cancel)
+        alert.addAction(okAction)
+        present(alert,animated: true,completion: nil)
+    }
+    
+    
+    @IBAction func Backbutton_action(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func set_persent(_ sender: UISegmentedControl) {
+        switch Control_persent.selectedSegmentIndex {
+        case 0:
+            showPersent = 0
+            print("0")
+        case 1:
+            showPersent = 1
+            print("1")
+        default: return
+        }
+    }
+    func check()
+    {
+        if (H1 != "")
+        {
+            h1 = Int(H1)!
+            m1 = 0
+            s1 = 0
+        }
+        if (H2 != "")
+        {
+            h2 = Int(H2)!
+            m2 = 0
+            s2 = 0
+        }
+        if (M1 != "")
+        {
+            if(H1 == "")
+            {
+                h1 = 0
+            }
+            m1 = Int(M1)!
+            s1 = 0
+        }
+        if (M2 != "")
+        {
+            if(H2 == "")
+            {
+                h2 = 0
+            }
+            m2 = Int(M2)!
+            s2 = 0
+        }
+        if (S1 != "")
+        {
+            if(H1 == "")
+            {
+                h1 = 0
+            }
+            if(M1 == "")
+            {
+                m1 = 0
+            }
+            s1 = Int(S1)!
+        }
+        if (S2 != "")
+        {
+            if(H2 == "")
+            {
+                h2 = 0
+            }
+            if(M2 == "")
+            {
+                m2 = 0
+            }
+            s2 = Int(S2)!
+        }
+    }
+    
+    func getHMS1()
+    {
+        s1 = allTime%60
+        h1 = allTime/3600
+        m1 = allTime/60 - h1*60
+    }
+    
+    func getHMS2()
+    {
+        s2 = second%60
+        h2 = second/3600
+        m2 = second/60 - h2*60
+    }
+    
+    func printTime(temp : Int) -> String
+    {
+        let S = temp%60
+        let H = temp/3600
+        let M = temp/60 - H*60
+        
+        let stringS = S<10 ? "0"+String(S) : String(S)
+        let stringM = M<10 ? "0"+String(M) : String(M)
+        
+        let returnString  = String(H) + ":" + stringM + ":" + stringS
+        return returnString
+    }
+    
+    func SET_action()
+    {
+        UserDefaults.standard.set(second, forKey: "second")
+        UserDefaults.standard.set(allTime, forKey: "allTime")
+        UserDefaults.standard.set(showPersent, forKey: "showPersent")
+        print("set complite")
+        setViewControllerDelegate.updateViewController()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
