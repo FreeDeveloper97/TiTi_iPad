0acb16cfda41559f65b122280370fc4de735fdff
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-07-11 19:38:37 +0900

    7/11 refector : 갤러리 최종디자인틀

--- Project_Timer/Test/testTodayViewController.swift
+++ Project_Timer/Test/testTodayViewController.swift
@@ -17,9 +17,9 @@ class testTodayViewController: UIViewController {
     @IBOutlet var selectDay: UILabel!
     @IBOutlet var selectDayBgView: UIView!
     
+    var staticHeight: CGFloat = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setRadius()
         setShadow(innerView)
         setShadowDayBgView()
@@ -28,8 +28,16 @@ class testTodayViewController: UIViewController {
         
     }
     
-    @IBAction func saveImage(_ sender: Any) {
-        saveImage(frame!)
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    @IBAction func saveImage(_ sender: UIButton) {
+        saveImage(view)
         showAlert()
     }
 }
@@ -74,7 +82,48 @@ extension testTodayViewController {
     }
     
     func saveImage(_ getFrame: UIView) {
+        let width: Double = Double(getFrame.bounds.width)
+        let height: Double = Double(getFrame.bounds.height)
         let img = UIImage.init(view: getFrame)
-        UIImageWriteToSavedPhotosAlbum(img, nil, nil, nil)
+        if(height > width) {
+            UIImageWriteToSavedPhotosAlbum(img, nil, nil, nil)
+        } else {
+            let rectImg = cropToBounds(image: img, width: width, height: height)
+            UIImageWriteToSavedPhotosAlbum(rectImg, nil, nil, nil)
+        }
     }
+    
+    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+
+            let cgimage = image.cgImage!
+            let contextImage: UIImage = UIImage(cgImage: cgimage)
+            let contextSize: CGSize = contextImage.size
+            var posX: CGFloat = 0.0
+            var posY: CGFloat = 0.0
+            var cgwidth: CGFloat = CGFloat(width)
+            var cgheight: CGFloat = CGFloat(height)
+
+            // See what size is longer and create the center off of that
+            if contextSize.width > contextSize.height {
+                posX = ((contextSize.width - contextSize.height) / 2)
+                posY = 0
+                cgwidth = contextSize.height
+                cgheight = contextSize.height
+            } else {
+                posX = 0
+                posY = ((contextSize.height - contextSize.width) / 2)
+                cgwidth = contextSize.width
+                cgheight = contextSize.width
+            }
+
+            let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+
+            // Create bitmap image from context using the rect
+            let imageRef: CGImage = cgimage.cropping(to: rect)!
+
+            // Create a new image based on the imageRef and rotate back to the original orientation
+            let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+
+            return image
+        }
 }
